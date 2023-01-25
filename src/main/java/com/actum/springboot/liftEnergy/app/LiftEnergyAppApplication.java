5c45f79d63fe5cc62ef0bf9151a1ec755be750f0
Sebastian <sebastian.rincon@actum.com.co>
2023-01-25 12:03:16 -0500

    first commit

---
+++ src/main/java/com/actum/springboot/liftEnergy/app/LiftEnergyAppApplication.java
@@ -0,0 +1,13 @@
+package com.actum.springboot.liftEnergy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftEnergyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftEnergyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/actum/springboot/liftEnergy/app/LiftEnergyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actum.springboot.liftEnergy.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftEnergyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
