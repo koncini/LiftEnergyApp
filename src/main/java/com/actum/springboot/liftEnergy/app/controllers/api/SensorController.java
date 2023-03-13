b5275c196849506b58c991129fa396a774c34328
Sebastian <sebastian.rincon@actum.com.co>
2023-03-13 09:56:57 -0500

    generando metodos para graficar

---
+++ src/main/java/com/actum/springboot/liftEnergy/app/controllers/api/SensorController.java
@@ -0,0 +1,10 @@
+package com.actum.springboot.liftEnergy.app.controllers.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("api Sensor")
+@RequestMapping("/api/sensor")
+public class SensorController {
+
+}
